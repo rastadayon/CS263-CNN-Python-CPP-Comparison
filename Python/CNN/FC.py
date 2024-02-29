e8174a3444e51984bdf65614d42658c3112f47f2
Rasta Tadayon Tahmasebi <rastadayon@gmail.com>
2024-02-29 13:25:46 -0800

    Add FC init class

---
+++ Python/CNN/FC.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class Linear:
+    def __init__(
+        self,
+        input
+    )
+
+class FC:
+    def __init__(
+        self,
+        input_dim,
+        hidden_sizes,
+        
+        bias = 1.0,
+    )
--- Python/utils/activations.py
+++ Python/utils/activations.py
@@ -8,3 +8,6 @@ def leaky_relu(x, alpha=0.001):
 
 def de_leaky_relu(x, alpha=0.001):
     return np.where(x < 0, alpha, x)
+
+def sigmoid(self, x):
+    return 1 / (1 + np.exp(-x))
